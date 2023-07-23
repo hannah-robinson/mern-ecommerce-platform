3e04f68d576cd733b6c2e8c16494a994ddda786d
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-07-23 17:24:55 +1200

    Add custom styles and logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/mernshop-logo.webp'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>MERNshop</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='MERNshop' />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
