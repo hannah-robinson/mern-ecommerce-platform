0d4f123bf7055c646d8358c68b21db520b228dd2
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-07-29 18:32:54 +1200

    Add MongDB and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config() // putting it here because bc we have to call config method before using any of the variables
+import connectDB from './config/db.js'
 import products from './data/products.js'
 const port = process.env.PORT || 5001
 // use port in .env file or 5000 as a fallback if .env not found
+
+connectDB() // connect to  MongoDB
 
 const app = express()
 
