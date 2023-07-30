8584a3872d1fe72f736b94a4a2f5ecca42baf7c7
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-07-30 13:06:01 +1200

    Move API routes to routes directory

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config() // putting it here because bc we have to call config method before using any of the variables
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5001
 // use port in .env file or 5000 as a fallback if .env not found
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(port, () =>
   console.log(`Server running on port http://localhost:${port}`)
