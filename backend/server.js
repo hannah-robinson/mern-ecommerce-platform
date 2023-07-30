c9f65f81e1329f6d50e00979876466d2dc413490
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-07-30 15:56:17 +1200

    Create custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config() // putting it here because bc we have to call config method before using any of the variables
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5001
 // use port in .env file or 5001 as a fallback if .env not found
@@ -15,6 +16,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () =>
   console.log(`Server running on port http://localhost:${port}`)
